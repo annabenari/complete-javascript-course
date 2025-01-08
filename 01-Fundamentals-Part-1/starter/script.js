7b6af5c6276362a95e069258f184f1bbd07d5dbc
annabenari <annabenari@me.com>
2025-01-08 17:03:27 +0000

    assignment one and notes for variables, values and console logs

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+let js = "amazing";
+// This comes up on the webpage
+// if (js === "amazing") alert("js is amazing");
+// This comes up on the webpage console
+console.log(3 + 5 + 7 + 9);
+
+// To connect this to the HTML file i added the following to the html:
+// <script src="script.js"></script>
+
+// Values and Variables
+//These are all values
+console.log("Anna");
+console.log("25");
+
+// This is a vairbale
+let first_name = "Anna";
+console.log(first_name);
+
+//Assignment One
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = "England";
+let continent = "Europe";
+let population = 4;
+
+console.log(country);
+console.log(continent);
+console.log(population);
