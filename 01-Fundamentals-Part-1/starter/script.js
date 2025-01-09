a07290df0162e31652a1a3d5c521aa68bc3a709a
annabenari <annabenari@me.com>
2025-01-09 19:18:37 +0000

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -28,3 +28,25 @@ let population = 4;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Primative data types
+let number = 23;
+let string = "hello";
+let booleon = True; //or False
+//undefined
+let children;
+null;
+//Symbol= that cannot be changed e.g. PI
+let PI = 3.14;
+//BigInt = larger ineregars than the data type can hold
+
+//typeof tells you what type of data type it is
+console.log(typeof true); // this comes out us booleon
+console.log(typeof 34); // this comes out as int
+
+//Reassigning a vairable
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+javaScriptIsFun = false; //here i am reassigning , dont need to put let again. I changed it to a different booleon
+console.log(javaScriptIsFun);
+javaScriptIsFun = 22; //Here i changed it to a different data type from a booleon to a Int
